8b8b61e5965779d9bd88b1e56f8f46e1b1e75572
crassbrasil <147461200+crassbrasil@users.noreply.github.com>
2025-04-16 13:18:31 -0300

    Update pix.php

--- pix.php
+++ pix.php
@@ -1,7 +1,7 @@
 <?php
 // Substitua os valores abaixo com suas credenciais e endpoint reais
 $secretKey = 'd2ff7bd819cc03e0b9902b1694b97922';
-$endpoint = 'https://crassbrasil.github.io/pix/'; // edite para o endpoint real
+$endpoint = 'https://api-production-0feb.up.railway.app'; // edite para o endpoint real
 
 header('Content-Type: application/json');
 
